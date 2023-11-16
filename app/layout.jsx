a3e1372d39e5c036222b5c5013aebc53479c2f51
PerpetuumG <perpetuumg@gmail.com>
2023-11-16 13:50:26 +0200

    final project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,28 @@
-import '@/styles/globals.css';
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-  title: 'Promptopia',
-  description: 'Discover & Share AI Prompts',
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang='en'>
-      <body>
-        <div className={'main'}>
-          <div className={'gradient'} />
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
         </div>
 
-        <main className={'app'}>{children}</main>
-      </body>
-    </html>
-  );
-}
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
